975ad07790abdb68282be9718dd87ca4bb52ce39
Pepperfudge <Chloeel@gmail.com>
2013-08-08 15:37:03 -0700

    Create Move.java

---
+++ Move.java
@@ -0,0 +1,10 @@
+// the new Move class returns and block and a string
+public class Move {
+  public Block myBlock;
+	public String oneDirection;
+
+	public Move(Block b, String d) {
+		myBlock = b;
+		oneDirection = d;
+	}
+}
