f0c128fd41c9eb1026becd09a742905918aca252
Pepperfudge <Chloeel@gmail.com>
2013-08-10 01:55:41 -0700

    Update Move.java

--- Move.java
+++ Move.java
@@ -2,29 +2,30 @@
 public class Move {
   public Block myBlock;
 	public String oneDirection;
+	private String repr;
 
 	public Move(Block b, String d) {
 		myBlock = b;
 		oneDirection = d;
+		repr = "";
+		int newX, newY;
+		if (oneDirection.equals("up")) {
+			newX = ((int) myBlock.getTopLeft().x) - 1;
+			newY = ((int) myBlock.getTopLeft().y);
+		} else if (oneDirection.equals("down")) {
+			newX = ((int) myBlock.getTopLeft().x) + 1;
+			newY = ((int) myBlock.getTopLeft().y);
+		} else if (oneDirection.equals("left")) {
+			newX = ((int) myBlock.getTopLeft().x);
+			newY = ((int) myBlock.getTopLeft().y) - 1;
+		} else {
+			newX = ((int) myBlock.getTopLeft().x);
+			newY = ((int) myBlock.getTopLeft().y) + 1;
+		}
+		repr = myBlock.getTopLeft().x + " " + myBlock.getTopLeft().y + " " + newX + " " + newY;
 	}
 	
 	public String toString() {
-		String rtn = "";
-		int newX, newY;
-		if (oneDirection.equals("up")) {
-			newX = ((int) myBlock.getTopLeft().getX()) - 1;
-			newY = ((int) myBlock.getTopLeft().getY());
-		} else if (oneDirection.equals("down")) {
-			newX = ((int) myBlock.getTopLeft().getX()) + 1;
-			newY = ((int) myBlock.getTopLeft().getY());
-		} else if (oneDirection.equals("left")) {
-			newX = ((int) myBlock.getTopLeft().getX());
-			newY = ((int) myBlock.getTopLeft().getY()) - 1;
-		} else {
-			newX = ((int) myBlock.getTopLeft().getX());
-			newY = ((int) myBlock.getTopLeft().getY()) + 1;
-		}
-		rtn = myBlock.getTopLeft().getX() + " " + myBlock.getTopLeft().getY() + " " + newX + " " + newY;
-		return rtn;
+		return repr;
 	}
 }
