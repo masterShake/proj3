4e02b6c7bcc70adcdd224acc5c8094ac73058918
phoebelin <phoebe.lin93@gmail.com>
2013-08-09 21:20:20 -0700

    Update Move.java

--- Move.java
+++ Move.java
@@ -7,4 +7,24 @@ public class Move {
 		myBlock = b;
 		oneDirection = d;
 	}
+	
+	public String toString() {
+		String rtn = "";
+		int newX, newY;
+		if (oneDirection.equals("up")) {
+			newX = ((int) myBlock.getTopLeft().getX()) - 1;
+			newY = ((int) myBlock.getTopLeft().getY());
+		} else if (oneDirection.equals("down")) {
+			newX = ((int) myBlock.getTopLeft().getX()) + 1;
+			newY = ((int) myBlock.getTopLeft().getY());
+		} else if (oneDirection.equals("left")) {
+			newX = ((int) myBlock.getTopLeft().getX());
+			newY = ((int) myBlock.getTopLeft().getY()) - 1;
+		} else {
+			newX = ((int) myBlock.getTopLeft().getX());
+			newY = ((int) myBlock.getTopLeft().getY()) + 1;
+		}
+		rtn = myBlock.getTopLeft().getX() + " " + myBlock.getTopLeft().getY() + " " + newX + " " + newY;
+		return rtn;
+	}
 }
